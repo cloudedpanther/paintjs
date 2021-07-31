de6e016dfaed39fd9276b0ddfe593d6244356a6e
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-07-31 22:13:23 +0900

    saving canvas
    completed!!

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ const ctx = canvas.getContext('2d');
 const colors = document.querySelectorAll('.controls__color');
 const range = document.querySelector('.controls__range .range');
 const mode = document.querySelector('.mode');
+const saveBtn = document.querySelector('.save');
 
 const INITIAL_COLOR = '#2c2c2c';
 
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 5;
@@ -26,10 +29,18 @@ function handleMouseMove(event) {
     const y = event.offsetY;
     if(!painting) {
         ctx.beginPath();
-        ctx.moveTo(x, y);
+        console.log(x, y);
     } else {
         ctx.lineTo(x, y);
         ctx.stroke();
+    }
+}
+
+function handleMouseDown(event) {
+    const mouseWhich = event.which;
+    // left mouse button down
+    if(mouseWhich === 1) {
+        startPainting();
     }
 }
 
@@ -67,14 +78,26 @@ function handleCanvasClick() {
     }
 }
 
+function handleContextMenu(event) {
+    event.preventDefault();
+}
+
+function handleSaveBtnClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = "paintjs_export";
+    link.click();
+}
+
 function init() {
     if(canvas) {
         canvas.addEventListener("mousemove", handleMouseMove);
-        canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("mousedown", handleMouseDown);
         document.addEventListener("mouseup", stopPainting);
-        // canvas.addEventListener("mouseleave", stopPainting);
         canvas.addEventListener("mouseenter", handleMouseEnter);
         canvas.addEventListener("click", handleCanvasClick);
+        canvas.addEventListener("contextmenu", handleContextMenu);
     }
 
     Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -85,6 +108,10 @@ function init() {
 
     if(mode) {
         mode.addEventListener("click", handleModeToggle);
+    }
+
+    if(saveBtn) {
+        saveBtn.addEventListener("click", handleSaveBtnClick);
     }
 }
 
