ef5c961289ad255785418abdd684e80aaee7f5af
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-07-31 20:20:08 +0900

    filling mode
    filling mode toggle button
    filling canvas with click only on filling mode

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@ const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext('2d');
 const colors = document.querySelectorAll('.controls__color');
 const range = document.querySelector('.controls__range .range');
+const mode = document.querySelector('.mode');
 
-ctx.strokeStyle = '#2c2c2c';
+const INITIAL_COLOR = '#2c2c2c';
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -37,11 +42,29 @@ function handleMouseEnter(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
     const brushSize = event.target.value;
     ctx.lineWidth = brushSize;
+}
+
+function handleModeToggle() {
+    if(filling) {
+        filling = false;
+        mode.innerText = 'Fill';
+    } else {
+        filling = true;
+        mode.innerText = 'Paint';
+    }
+    
+}
+
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 }
 
 function init() {
@@ -51,12 +74,17 @@ function init() {
         document.addEventListener("mouseup", stopPainting);
         // canvas.addEventListener("mouseleave", stopPainting);
         canvas.addEventListener("mouseenter", handleMouseEnter);
+        canvas.addEventListener("click", handleCanvasClick);
     }
 
     Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 
     if(range) {
         range.addEventListener("input", handleRangeChange);
+    }
+
+    if(mode) {
+        mode.addEventListener("click", handleModeToggle);
     }
 }
 
